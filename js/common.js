cb617de45b27eae323f1343fd3ec8759620b2a03
unknown <hgkim@rianag.com>
2024-07-03 17:54:11 +0900

    구조 1~3까지 완성

--- js/common.js
+++ js/common.js
@@ -7,40 +7,43 @@ const urlParams = new URL(location.href).searchParams;
 
 $(document).ready(function(){
 
-    $.ajax({
-		url:'./pages/page_0'+scene+'.html', //호출할 파일 경로
-		method:'get',
-		success: function(html){
-			$('.wrap').html(html); //호출한 파일을 대상 요소에 html로 담음
-		}, complete: function(){
-		}, error: function(){
-			// $('.wrap').append('<strong>Error!!</strong>');
-		}
-	});
+    // $('.wrap').css('opacity',1);
+    setTimeout(function(){
+        $('.wrap').css('opacity',1);
+    },350);
+    link.ajax(Number(scene));
+
 });
 
 const link = {
-	prev:()=>{
-		console.log('p:'+scene);
-		if(scene > 1){
-			link.ajax(scene-1);
-			location.href = '?scene='+(Number(scene)-1);
-		}
-	},
-	next:()=>{
-		link.ajax(scene+1);
-		location.href = '?scene='+(Number(scene)+1);
-	},
-	ajax:(scene)=>{
-		$.ajax({
-			url:'./pages/page_0'+scene+'.html', //호출할 파일 경로
-			method:'get',
-			success: function(html){
-				$('.wrap').html(html); //호출한 파일을 대상 요소에 html로 담음
-			}, complete: function(){
-			}, error: function(){
-				// $('.wrap').append('<strong>Error!!</strong>');
-			}
-		});
-	}
+    prev:()=>{
+        if(scene > 1){
+            link.ajax(Number(scene)-1);
+            $('.wrap').off('transitionend');
+            $('.wrap').css('opacity',0).on('transitionend', function(){
+                location.href = '?scene='+(Number(scene)-1);
+            });
+        }
+    },
+    next:()=>{
+        link.ajax(Number(scene)+1);
+        $('.wrap').off('transitionend');
+        $('.wrap').css('opacity',0).on('transitionend', function(){
+            location.href = '?scene='+(Number(scene)+1);
+        });
+    },
+    ajax:(scene)=>{
+        $.ajax({
+            url:'./pages/page_0'+scene+'.html', //호출할 파일 경로
+            method:'get',
+            success: function(html){
+                setTimeout(function(){
+                    $('.wrap').html(html); //호출한 파일을 대상 요소에 html로 담음
+                },350);
+            }, complete: function(){
+            }, error: function(){
+                // $('.wrap').append('<strong>Error!!</strong>');
+            }
+        });
+    }
 }
